fd7574207a68bbd3eea7052ed0a0fa4f867b7b6c
Chad Michel <chadmichel@me.com>
2014-08-08 17:01:38 -0500

    start something here...

---
+++ ChadsCloudSyncWin/ChadsCloudSyncWin/SyncSetup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChadsCloudSyncWin
+{
+    public class SyncSetup
+    {
+        public int Id { get; set; }
+        public string LocalPath { get; set; }
+        public string CloudPath { get; set; }
+        public string CloudKey { get; set; }
+        public string CloudSecret { get; set; }
+    }
+}
